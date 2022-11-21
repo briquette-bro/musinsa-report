7ab9860000acc1a96ace2c775e76725265632692
shlee <shlee@hoogi.com>
2022-11-21 21:11:41 +0900

    enum validation 관련 수정 - @JsonCreator 제거

--- src/main/java/com/musinsa/stock/model/dto/RequestDto.java
+++ src/main/java/com/musinsa/stock/model/dto/RequestDto.java
@@ -13,25 +13,16 @@ public class RequestDto {
     @Data
     @NoArgsConstructor
     public static class UpdateStockCommand {
-        @NotBlank(message = "상품명은 필수 입니다.")
         private String productName;
         @NotBlank(message = "상품 옵션명은 필수 입니다.")
         private String productOptionName;
-        @Min(value = 0, message = "수량은 0보다 커야 합니다.")
+        @Min(value = 1, message = "수량은 0보다 커야 합니다.")
         private int quantity;
-        @NotNull(message = "command는 INCREASE | DECREASE만 사용 가능 합니다.")
+        @NotNull(message = "command는 INCREASE | DECREASE 만 사용 가능 합니다.")
         private QuantityCommand command;
     }
 
     public enum QuantityCommand {
         INCREASE, DECREASE;
-        @JsonCreator
-        public static QuantityCommand getCodeByValue(final String value) {
-            for(QuantityCommand command: QuantityCommand.values()) {
-                if(command.name() == value)
-                    return command;
-            }
-            return null;
-        }
     }
 }
