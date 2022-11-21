ec839846e5bce01a26bb7ac60ffa95be75263649
shlee <shlee@hoogi.com>
2022-11-21 13:28:17 +0900

    프로젝트 생성 및 config file 작성

---
+++ src/main/java/com/musinsa/stock/MusinsaReportApplication.java
@@ -0,0 +1,13 @@
+package com.musinsa.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusinsaReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusinsaReportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/musinsa/stock/MusinsaReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.musinsa.stock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusinsaReportApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
